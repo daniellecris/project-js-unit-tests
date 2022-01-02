109a7f27a5a7d6aa0aa6db456853e76925593cde
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-01-02 10:55:11 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,17 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  //let mensagem = '';
+
+  if (typeof name !== 'string' && typeof age !== 'number') return undefined;
+
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+ // return mensagem;
+};
 
 module.exports = vqv;
